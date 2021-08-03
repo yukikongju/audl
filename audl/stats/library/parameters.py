8c71a160276655447bfb94e2495b794dea40e331
Emulie Chhor <emulie.chhor@hotmail.com>
2021-08-03 19:24:15 -0400

    added players, team and parameters information

---
+++ audl/stats/library/parameters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env/python
+
+class Division:
+    altantic = 'Atlantic'
+    central = 'Central'
+    west = 'West'
+    canada = 'Canada Cup'
+
+
+class StatisticsDescription:
+    YR = 'Year'
+    G = 'Games Played'
+    PP = 'Points Played'
+    TM = 'Team'
+    AST = 'Assists'
+    GLS = 'Goals'
+    BLK = 'Blocks'
+    PLUS_MINUS = 'Plus Minus'
+    CMP = 'Completions'
+    CMP_PERC = 'Completion Percentage'
+    TY = 'Throwing Yards'
+    RY = 'Receiving Yards'
+    HA = 'Hockey Assists'
+    T = 'Throwaways'
+    S = 'Stall'
+    C = 'Callahan'
+    D = 'Drops'
+    OPP = 'Offensive Points Played'
+    DPP = 'Defensive Points Played'
+    P = 'Pulls'
