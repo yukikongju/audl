8922bb85cb06ed31956a29074df841061c100009
Emulie Chhor <emulie.chhor@hotmail.com>
2021-08-04 23:41:54 -0400

    removed unused functions in playerprofile.py

--- audl/stats/endpoints/playerprofile.py
+++ audl/stats/endpoints/playerprofile.py
@@ -3,7 +3,6 @@
 import pandas as pd
 from audl.stats.endpoints._base import Endpoint
 from audl.stats.static import players
-from audl.stats.library.parameters import StatisticAbbreviation
 
 
 class PlayerProfile(Endpoint):
@@ -17,10 +16,6 @@ class PlayerProfile(Endpoint):
     def _get_endpoint(self):
         return players.find_player_id_from_full_name(self.full_name)
 
-    def get_all_regular_seasons(self):
-        dfs = pd.read_html(self.url)
-        return dfs[0]
-
     def _is_audl_player(self) -> bool:
         player_names = players.get_list_players_by_name()
         for name in player_names:
@@ -28,25 +23,9 @@ class PlayerProfile(Endpoint):
                 return True
         return False
 
-    def get_regular_season_by_year(self, year: int):
-        df_all = self.get_all_regular_seasons()
-        df_season = df_all.loc[df_all[StatisticAbbreviation.stat_col_year] == str(
-            year)]
-        if df_season is None:
-            raise Exception(f"{self.full_name} did not play in {year}")
-            #  return None
-        else:
-            return df_season
+    def get_regular_seasons_career(self) -> list:
+        dfs = pd.read_html(self.url)
+        return dfs[0]
 
-    def get_regular_season_career(self):
-        df = self.get_all_regular_seasons()
-        return df.loc[df[StatisticAbbreviation.stat_col_year] == 'Career']
-
-    def get_all_playoffs_stats(self):
-        pass
-
-    def get_playoff_by_year(self):
-        pass
-
-    def get_playoff_career(self):
+    def get_playoffs_career(self):
         pass
--- tests/stats/endpoints/test_playerprofile.py
+++ tests/stats/endpoints/test_playerprofile.py
@@ -6,14 +6,8 @@ from audl.stats.endpoints.playerprofile import PlayerProfile
 
 class TestPlayerProfile(unittest.TestCase):
 
-    def test_get_all_regular_seasons(self):
-        print(PlayerProfile("Rowan McDonnell").get_all_regular_seasons())
+    def test_get_regular_seasons_career(self):
+        print(PlayerProfile("Rowan McDonnell").get_regular_seasons_career())
 
     def test_is_audl_player(self):
         print(PlayerProfile("Rowan McDonnell")._is_audl_player())
-
-    def test_get_regular_season_by_year(self):
-        print(PlayerProfile("Rowan McDonnell").get_regular_season_by_year(2021))
-
-    def test_get_regular_season_career(self):
-        print(PlayerProfile("Rowan McDonnell").get_regular_season_career())
