4b189744bcc14956b70a8e36c4d4f42e4d141d2c
Emulie Chhor <emulie.chhor@hotmail.com>
2021-08-04 23:42:25 -0400

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,23 @@
-#!/usr/bin/env/python
+import setuptools
 
-from setuptools import setup, find_packages
-import codecs
-import os
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
-VERSION = '1.0.0'
-DESCRIPTION = 'AUDL API'
-LONG_DESCRIPTION = 'Unofficial AUDL API'
-
-setup(
+setuptools.setup(
     name="audl",
-    version=VERSION,
+    version="1.0.1",
     author="yukikongju",
-    author_email="temp@gmail.com",
-    description=DESCRIPTION,
+    author_email="yukikongju@outlook.com",
+    description="Unofficial audl api",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    install_requires=['lxml', 'html5lib', 'pandas'],
     url="https://github.com/yukikongju/audl",
-    include_package_data=True,
-    keywords=['audl'],
     classifiers=[
-        "Development Status :: 1 - Planning",
         "Programming Language :: Python :: 3",
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "audl"},
+    packages=setuptools.find_packages(where="audl"),
+    python_requires=">=3.6",
 )
